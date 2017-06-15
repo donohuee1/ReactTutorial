de9220a9eb0356a5a28d305a27e9dc774cac0f66
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-06-14 23:28:45 -0700

    added render function

--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,22 @@
+//component
+//react.createClass is most common react method. Takes options object as its one and only argument.  Where we can define the behaviour of our component
+var Greeter = React.createClass({
+  //the only thing that's required of react component is a render method
+  render: function () {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <p>This is from a component</p>
+      </div>
+    );
+  }
+});
+
 //function we call and pass 2 arguments.  First is piece of jsx and the second our element, the app element
 ReactDOM.render(
   // pass it someting to render (i.e. h1 tag)
-  <h1>Hello React!</h1>,
+  // <h1>Hello React!</h1>,
+  <Greeter/>,
   //and location to render it. Our node.
   document.getElementById('app')
 );
